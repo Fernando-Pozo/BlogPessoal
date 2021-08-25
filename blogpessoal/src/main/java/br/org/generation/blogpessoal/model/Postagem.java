87fbadf4b1d6d26da90fd1f67ed91895fea19bf6
Fernando-Pozo <fernandopozomqs@gmail.com>
2021-08-25 10:53:03 -0300

    Crud

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -17,22 +17,22 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-
+	//criar indetificador que gera automaticamente
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+	//forçar o usuario a digitar um titulo que os parametros seja de 5 a 100 e caso o usuatio nao digite exiba a mensagem
 	@NotNull(message = "O atributo título é obrigatorio!")
 	@Size(min = 5, max = 100, message = "O atributo título deve ter no minimo 5 e no maximo 100 caracteres")
 	private String titulo;
-	
+	//forçar o usuario a digitar um titulo que os parametros seja de 5 a 100 e caso o usuatio nao digite exiba a mensagem
 	@NotNull(message = "O atributo texto é obrigatorio!")
 	@Size(min = 5, max = 100, message = "O atributo texto deve ter no minimo 5 e no maximo 1000 caracteres")
 	private String texto;
-	
+	//prenche data pegando do sistemas
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date data = new java.sql.Date(System.currentTimeMillis());
-
+	private Date data = new java.sql.Date(System.currentTimeMillis()); //para add os milisegundos
+	//Get e settings 
 	public long getId() {
 		return id;
 	}
